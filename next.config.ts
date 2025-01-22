38fb21d2bbeb06c6722b73bd3283bbefa049fd67
Michael DeMarco <michaelfromyeg@gmail.com>
2025-01-21 22:41:16 -0800

    feat: deploy?

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Enable static exports
+  basePath: process.env.GITHUB_ACTIONS ? "/more-slackthemes" : "", // Adjust this to your repo name
+  images: {
+    unoptimized: true, // Required for static export
+  },
 };
 
 export default nextConfig;
