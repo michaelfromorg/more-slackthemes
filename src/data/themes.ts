fd6a7762dc0c5973ac0bba08364aa9b417dd39be
Michael DeMarco <michaelfromyeg@gmail.com>
2025-02-01 16:56:14 -0800

    feat: a better default theme

--- src/data/themes.ts
+++ src/data/themes.ts
@@ -7,4 +7,4 @@ import rawThemes from "./themes.json";
 export const themes: Theme[] = rawThemes.map(processTheme);
 
 // Export default theme for initial state
-export const defaultTheme = themes[0];
+export const defaultTheme = themes.find((theme) => theme.name === "Aubergine")!;
